1a10199016629f7f1eb48a1a1e41c9474c6d0263
Knot Hulk <knothulk@mi-c2.msk.21-school.ru>
2022-08-21 20:02:04 +0300

    print_graph

--- src/print_graph.c
+++ src/print_graph.c
@@ -1,1 +1,29 @@
 #include "print_graph.h"
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define eps 2. / 48.
+
+
+
+void print_graph(double* data) {
+    char A[OSY][OSX];
+    double y_step = (Y_UP - Y_DOWN) / OSY; 
+    for (int i = 0; i < OSX; i++) {
+        for (int j = 0; j < OSY; j++) {
+            if ((data[i] > Y_DOWN + j*y_step) && (data[i] < Y_DOWN + (j + 1) * y_step))
+                A[i][j] = '*';
+            else 
+                A[i][j] = '.';
+        }
+    }
+    for (int i = 0; i < OSX; i++) {
+        for (int j = 0; j < OSY; j++) {
+            printf("%lf", A[i][j]);
+        }
+        printf("\n");
+    }
+}
--- src/print_graph.h
+++ src/print_graph.h
@@ -1,8 +1,14 @@
 #ifndef SRC_PRINT_GRAPH_H_
 #define SRC_PRINT_GRAPH_H_
-
+#define OSX 80
+#define OSY 25
+#define X_LEFT 0
+#define X_RIGHT 4*M_PI
+#define Y_UP 1
+#define Y_DOWN -1
+#include <math.h>
 #include <stdio.h>
 
-void 
+void print_graph(double* data);
 
 #endif  // SRC_PRINT_GRAPH_H_
